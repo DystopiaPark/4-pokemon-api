998973c46b34b2bc000d9688d68adc28eace9dfe
Filip <68464364+DystopiaPark@users.noreply.github.com>
2023-02-11 21:59:08 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelector("#search").addEventListener("click", getPokemon);
+
+function getPokemon(e) {
+  const name = document.querySelector("#pokemonName").value;
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".pokemonBox").innerHTML = `
+     <div>
+          <img
+            src="${data.sprites.other["official-artwork"].front_default}"
+            alt="${data.name}"
+          />
+        </div>
+        <div class="pokemonInfo">
+          <h1>${data.name}</h1>
+          <p>Weight: ${data.weight}</p>
+        </div>
+     `;
+    })
+    .catch((err) => {
+      console.log("Pokemon not found", err);
+    });
+  e.preventDefault();
+}
